f5ca57244b1efac44bad0ee886e3322e55f40145
Victoriam2410 <estherrodas61@gmail.com>
2026-02-22 22:41:56 -0600

    creación de los primeros case, donde estan los perfiles del docente y estudiante.

--- RodasEsther_Ejercicio2/Program.cs
+++ RodasEsther_Ejercicio2/Program.cs
@@ -12,3 +12,33 @@ Console.WriteLine("Trae usb (S/N)");
 string traeUSB = Console.ReadLine();
 Console.WriteLine("Si trae USB preguntar si está acompañado (S/N)");
 string acompañado = Console.ReadLine();
+
+if (hora >= 0 && hora <= 24 && dia >= 1 && dia <= 7 && carnet == "S" || carnet == "N" & autorizacion == "S" || autorizacion == "N" && traeUSB == "S" || traeUSB == "N" && acompañado == "S" || acompañado == "N")
+{
+    switch (option)
+    {
+        case 1:
+            Console.WriteLine("Bienvenido estudiante");
+            if (hora >= 7 && hora <= 18 && dia >= 1 && dia <= 5)
+            {
+                Console.WriteLine("Tiene ingreso al laboratorio");
+            }
+            else
+            {
+                Console.WriteLine("No tiene ingreso al laboratorio ");
+            }
+
+            break;
+        case 2:
+            Console.WriteLine("Bienvenido docente.");
+            if (hora >= 7 && hora <= 18 && dia >= 1 && dia <= 5)
+            {
+                Console.WriteLine("Tiene ingreso al laboratorio.");
+            }
+            else
+            {
+                Console.WriteLine("No tiene ingreso al laboratorio.");
+            }
+            break;
+    }
+}
