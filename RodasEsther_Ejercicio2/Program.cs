a9bee2b2ad1c721ba95fe5a3f638f69a0a9565b2
Victoriam2410 <estherrodas61@gmail.com>
2026-02-22 23:08:15 -0600

    Corrección en los if, cambie algunos print

--- RodasEsther_Ejercicio2/Program.cs
+++ RodasEsther_Ejercicio2/Program.cs
@@ -1,16 +1,16 @@
-﻿Console.WriteLine("Seleccione una de las opciones: \n selecciones la opccion que desee \n1 Estudiante \n2.Docente \n3 Técnico IT \n4 Visitante ");
+﻿Console.WriteLine("Seleccione una de las opciones: \n selecciones la opcción que desee \n1 Estudiante \n2.Docente \n3 Técnico IT \n4 Visitante ");
 int option = int.Parse(Console.ReadLine());
-Console.WriteLine("ingrese hora");
+Console.WriteLine("Ingrese hora (0-23)h: ");
 int hora = int.Parse(Console.ReadLine());
-Console.WriteLine("Ingrese dia de la semana");
+Console.WriteLine("Ingrese día (1-7):");
 int dia = int.Parse(Console.ReadLine());
-Console.WriteLine("Ingrese carnet vigente (S/N)");
+Console.WriteLine("¿Carnet vigente? (S/N):");
 string carnet = Console.ReadLine();
-Console.WriteLine("Ingrese autorizacion escrita (S/N)");
+Console.WriteLine("¿Autorización escrita? (S/N):");
 string autorizacion = Console.ReadLine();
-Console.WriteLine("Trae usb (S/N)");
+Console.WriteLine("¿Trae USB? (S/N):");
 string traeUSB = Console.ReadLine();
-Console.WriteLine("Si trae USB preguntar si está acompañado (S/N)");
+Console.WriteLine("¿Está acompañado? (S/N):");
 string acompañado = Console.ReadLine();
 
 if (hora >= 0 && hora <= 24 && dia >= 1 && dia <= 7 && carnet == "S" || carnet == "N" & autorizacion == "S" || autorizacion == "N" && traeUSB == "S" || traeUSB == "N" && acompañado == "S" || acompañado == "N")
@@ -18,27 +18,41 @@ if (hora >= 0 && hora <= 24 && dia >= 1 && dia <= 7 && carnet == "S" || carnet =
     switch (option)
     {
         case 1:
-            Console.WriteLine("Bienvenido estudiante");
-            if (hora >= 7 && hora <= 18 && dia >= 1 && dia <= 5)
+            Console.WriteLine("Bienvenido Estudiante");
+            if (dia <= 5 && hora >= 7 && hora <= 18)
             {
-                Console.WriteLine("Tiene ingreso al laboratorio");
+                Console.WriteLine("Acceso permitido.");
             }
             else
             {
-                Console.WriteLine("No tiene ingreso al laboratorio ");
+                Console.WriteLine("Acceso denegado.");
             }
-
             break;
         case 2:
-            Console.WriteLine("Bienvenido docente.");
-            if (hora >= 7 && hora <= 18 && dia >= 1 && dia <= 5)
+            Console.WriteLine("Bienvenido Docente");
+            if (dia <= 5 && hora >= 7 && hora <= 18)
             {
-                Console.WriteLine("Tiene ingreso al laboratorio.");
+                Console.WriteLine("Acceso permitido, jornada laboral.");
             }
             else
             {
-                Console.WriteLine("No tiene ingreso al laboratorio.");
+                Console.WriteLine("Acceso denegado, no esta en su jornada laboral. ");
             }
+            break;
+        case 3: 
+            Console.WriteLine("Bienvenido Técnico IT");
+            if (hora < 18)
+            {
+                Console.WriteLine("Acceso permitido.");
+            }
+            else if (autorizacion == "S")
+                {
+                    Console.WriteLine("Acceso permitido.");
+                }
+                else
+                {
+                    Console.WriteLine("Acceso denegado.");
+                }
             break;
     }
 }
